5372766ac65a078cf3b409a26cbaf96bfc5548b4
guuhx97 <gustavorobertotb@outlook.com>
2020-11-15 19:21:04 -0300

    :construction: Chore: continuing nodejs studies

---
+++ nivel-02/back-end-desafio/src/services/DeleteTransactionService.ts
@@ -0,0 +1,16 @@
+import { getCustomRepository } from 'typeorm';
+import TransactionRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
+
+class DeleteTransactionService {
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const transaction = await transactionRepository.findOne(id);
+    if (!transaction) {
+      throw new AppError('Transaction does not exist');
+    }
+    await transactionRepository.remove(transaction);
+  }
+}
+
+export default DeleteTransactionService;
