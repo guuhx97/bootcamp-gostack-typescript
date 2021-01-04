1212becffde9c638fc22642cc3676b4e51dd9544
guuhx97 <gustavorobertotb@outlook.com>
2021-01-03 21:52:59 -0300

    :construction: Feat: Ends login page

--- nivel-03/gobarber-web/src/App.tsx
+++ nivel-03/gobarber-web/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import SignIn from './pages/SignIn';
 
 import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <>
+      <SignIn />
       <GlobalStyle />
-      <h1>Hello WOrld</h1>
     </>
   );
 }
--- nivel-03/gobarber-web/src/styles/global.ts
+++ nivel-03/gobarber-web/src/styles/global.ts
@@ -2,16 +2,16 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   * {
-    margin: 0;
-    padding: 0;
     box-sizing: border-box;
+    margin: 0;
     outline: 0;
+    padding: 0;
   }
 
-  body{ 
+  body{
+    -webkit-font-smoothing: antialiased;
     background: #312e38;
     color: #fff;
-    -webkit-font-smoothing: antialiased;
   }
 
   body, input, button {
