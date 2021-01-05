f332b9ebdcf7eca93c246416367ad43035595f88
guuhx97 <gustavorobertotb@outlook.com>
2021-01-04 22:11:12 -0300

    :white_check_mark: Feat: Ends validation SignIn and SignUp pages

--- nivel-03/gobarber-web/src/components/Input/index.tsx
+++ nivel-03/gobarber-web/src/components/Input/index.tsx
@@ -1,8 +1,11 @@
 import React, { InputHTMLAttributes, useEffect, useRef, useState, useCallback } from 'react';
 import { IconBaseProps } from 'react-icons';
 import { useField } from '@unform/core';
+import {FiAlertCircle} from 'react-icons/fi';
 
-import { Container } from './styles';
+import Tooltip from '../Tooltip';
+
+import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
   name: string;
@@ -33,7 +36,7 @@ const Input: React.FC<InputProps> = ({name, icon: Icon, ...rest}) => {
   }, [fieldName, registerField])
 
   return (
-    <Container isFocused={isFocused} isFilled={isFilled}>
+    <Container isErrored={!!error} isFocused={isFocused} isFilled={isFilled}>
       {Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
@@ -42,6 +45,11 @@ const Input: React.FC<InputProps> = ({name, icon: Icon, ...rest}) => {
         ref={inputRef}
         {...rest}
       />
+      {error && (
+        <Error title={error}>
+          <FiAlertCircle color="#c53030" size={20} />
+        </Error>
+      )}
     </Container>
   );
 };
