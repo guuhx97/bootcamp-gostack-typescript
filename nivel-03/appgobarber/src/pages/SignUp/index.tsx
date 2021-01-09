b4e584b35c74d543747eab5a46d92581bff57f1f
guuhx97 <gustavorobertotb@outlook.com>
2021-01-08 22:43:55 -0300

    :white_check_mark: Feat: Finish SignIn and SignUp screen

--- nivel-03/appgobarber/src/pages/SignUp/index.tsx
+++ nivel-03/appgobarber/src/pages/SignUp/index.tsx
@@ -1,10 +1,118 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, { useCallback, useRef } from 'react';
+import { Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import Icon from 'react-native-vector-icons/Feather';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+import logoImg from '../../assets/logo.png';
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+import getValidationErrors from '../../utils/getValidationErrors';
 
-// import { Container } from './styles';
+interface ISignInFormData {
+  email: string;
+  password: string;
+}
 
 const SignUp: React.FC = () => {
-  return <View />;
+  const formRef = useRef<FormHandles>(null);
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordInputText = useRef<TextInput>(null);
+  const navigation = useNavigation();
+
+  const handleSubmit = useCallback(async (data: ISignInFormData) => {
+    try {
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'No mínimo 6 digito'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false
+      });
+
+      // await api.post('/users', data);
+      // history.push('/');
+
+      Alert.alert('Cadastro realizado', 'Você já pode fazer seu logon no GoBarber');
+    }catch(err){
+      if(err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err);
+        formRef.current?.setErrors(errors);
+        Alert.alert('Erro no cadastro', 'Ocorreu um erro ao fazer cadastro, tente novamente');
+        return;
+      }
+
+    }
+  }, []);
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        style={{ flex: 1}}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImg} />
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+            <Form ref={formRef} onSubmit={handleSubmit}>
+              <Input
+                autoCapitalize="words"
+                name="name"
+                icon="user"
+                placeholder="Nome"
+                returnKeyType="next"
+                onSubmitEditing={ () => {
+                  emailInputRef.current?.focus();
+                }}
+              />
+              <Input
+                ref={emailInputRef}
+                name="email"
+                icon="mail"
+                placeholder="E-mail"
+                autoCorrect={false}
+                autoCapitalize="none"
+                keyboardType="email-address"
+                returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputText.current?.focus();
+                }}
+              />
+              <Input
+                ref={passwordInputText}
+                name="password"
+                icon="lock"
+                placeholder="Password"
+                secureTextEntry
+                textContentType="newPassword"
+                returnKeyType="send"
+                onSubmitEditing={() => { formRef.current?.submitForm()}}
+              />
+            </Form>
+
+            <Button onPress={() => { formRef.current?.submitForm()}}>Cadastrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+      <BackToSignIn onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={20} color="#ffffff" />
+        <BackToSignInText>Voltar para logon</BackToSignInText>
+      </BackToSignIn>
+    </>
+  );
 };
 
 export default SignUp;
